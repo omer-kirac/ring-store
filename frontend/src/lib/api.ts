1f4484fb7e7e12d84924f6940faf105ac6e4edac
omer-kirac <omer.kirac@example.com>
2025-04-16 15:55:09 +0300

    API_URL değişkeninden /api ön eki kaldırıldı

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { Product } from './types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
 
 export async function fetchProducts(
   filters?: {
